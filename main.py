0b0a9de6e8cb9989bc16633debc3fae463d1583b
saksham1506 <sakshamarihantjain@gmail.com>
2025-05-17 01:39:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -48,5 +48,9 @@ def webhook():
 
     return {"status": status, "response": res}
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
